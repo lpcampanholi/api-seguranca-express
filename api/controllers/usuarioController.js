86fbc42f899a00b2dae4f5e3ca6cf2a8295b55b8
Luís Plinio <lpcampanholi@gmail.com>
2025-02-15 16:48:32 -0300

    Criação de usuário

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -1,5 +1,17 @@
-class UsuarioController {
+const UsuarioService = require("../services/usuarioService");
 
+const usuarioService = new UsuarioService();
+
+class UsuarioController {
+  static async cadastrar(req, res) {
+    const { nome, email, senha } = req.body;
+    try {
+      const usuario = usuarioService.cadastrar({ nome, email, senha });
+      res.status(201).send(usuario);
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  }
 }
 
 module.exports = UsuarioController;
--- api/services/usuarioService.js
+++ api/services/usuarioService.js
@@ -1,3 +1,32 @@
-class UsuarioService {}
+const database = require("../models");
+const { hash } = require("bcryptjs");
+const uuid = require("uuid");
+
+class UsuarioService {
+  async cadastrar(dto) {
+    const usuario = await database.usuarios.findOne({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (usuario) {
+      throw new Error("Usuário já cadastrado");
+    }
+
+    try {
+      const senhaHasheada = await hash(dto.senha, 8);
+      const novoUsuario = await database.usuarios.create({
+        id: uuid.v4(),
+        nome: dto.nome,
+        email: dto.email,
+        senha: senhaHasheada,
+      });
+      return novoUsuario;
+    } catch (error) {
+      throw new Error("Erro ao cadastrar Usuário");
+    }
+  }
+}
 
 module.exports = UsuarioService;
